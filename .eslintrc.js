f7205318560f2615c228a51a331321576feb3e24
Karl-Erik Gustafsson <ke.gustafsson@gmail.com>
2021-02-17 21:14:05 +0200

    fix: eslint problems

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+    browser: true,
+    jquery: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    'linebreak-style': 0,
+    'no-console': 0,
+    'func-names': 0,
+    'prefer-destructuring': 0,
+    'one-var-declaration-per-line': 0,
+    'one-var': 0,
+    'no-plusplus': 0,
+    'no-loop-func': 0,
+    'no-use-before-define': 0,
+    'no-param-reassign': 0,
+  },
+};
--- index.js
+++ index.js
@@ -1,149 +1,146 @@
-const dgram = require("dgram");
+const dgram = require('dgram');
 const Parser = require('@signalk/nmea0183-signalk');
 
 module.exports = function (app) {
-  const plugin = {}
-  plugin.id = 'IEC61162-450-NMEA0183-client-plugin'
-  plugin.name = 'IEC61162-450 to NMEA0183 client'
-  plugin.description = plugin.name
-  const setStatus = app.setPluginStatus || app.setProviderStatus;
+  const plugin = {};
+  plugin.id = 'iec61162-450-nmea0183-client-plugin';
+  plugin.name = 'IEC61162-450 to NMEA0183 client';
+  plugin.description = plugin.name;
+  // const setStatus = app.setPluginStatus || app.setProviderStatus; // Not used yet
 
   let numberMulticast = null;
-  let socketMulticast = [];
+  const socketMulticast = [];
   let socketUdp = [];
-  const parser = new Parser()
+  const parser = new Parser();
 
   plugin.start = function (options) {
     numberMulticast = Object.keys(options.multicast).length;
-    app.debug(`Number of configs: ${numberMulticast}`)
+    app.debug(`Number of configs: ${numberMulticast}`);
     app.debug(options.interfaceIP);
     let multicast;
-    let multicastCounter = 0;
+    let counter = 0;
 
-    if(options.sendAddress && options.sendPort) {
-      socketUdp = dgram.createSocket({ type: "udp4", reuseAddr: true });
+    if (options.sendAddress && options.sendPort) {
+      socketUdp = dgram.createSocket({ type: 'udp4', reuseAddr: true });
     }
 
+    let i;
     for (i = 0; i < numberMulticast; i++) {
       multicast = options.multicast[i];
-      socketMulticast[i] = dgram.createSocket({ type: "udp4", reuseAddr: true });
+      socketMulticast[i] = dgram.createSocket({ type: 'udp4', reuseAddr: true });
       socketMulticast[i].bind(multicast.multicastPort, () => {
-        socketMulticast[k].addMembership(options.multicast[k].multicastAddress);
-        app.debug(options.multicast[k].multicastAddress);
-        app.debug(options.multicast[k].multicastPort);
-        app.debug(options.interfaceIP);        
-        multicastCounter = multicastCounter + 1;
+        socketMulticast[counter].addMembership(options.multicast[counter].multicastAddress);
+        app.debug(options.multicast[counter].multicastAddress);
+        app.debug(options.multicast[counter].multicastPort);
+        app.debug(options.interfaceIP);
+        counter += 1;
       });
 
-      if(socketMulticast[i]) {
-        socketMulticast[i].on("message", function(message) {
+      if (socketMulticast[i]) {
+        socketMulticast[i].on('message', (message) => {
           message = message.toString('utf8');
           app.debug(message);
-          //console.log(JSON.stringify(message, null, 2)); //For debugging JSON
+          // console.log(JSON.stringify(message, null, 2)); //For debugging JSON
           nmeaParser(message);
-          if(socketUdp) {
-            udpSend(message,options.sendAddress,options.sendPort);
+          if (socketUdp) {
+            udpSend(message, options.sendAddress, options.sendPort);
           }
-          if(options.sendNmeaOut) {
-            nmeaOut(message,options.sendNmeaOut)
+          if (options.sendNmeaOut) {
+            nmeaOut(message, options.sendNmeaOut);
           }
         });
       }
     }
-  }
-  
-  function sendMessage(message,host,port,i) {
-    udpSend(message,host,port,i);
-  }
+  };
 
-  function udpSend(message,host,port) {
-    socketUdp.send(message,port,host,function(error){
-      if(error){
+  function udpSend(message, host, port) {
+    socketUdp.send(message, port, host, (error) => {
+      if (error) {
         socketUdp.close();
       }
     });
   }
-  
-  function nmeaOut(message,sendNmeaOut) {
-    app.emit(sendNmeaOut, message.replace(/\r?\n|\r/, " "));
+
+  function nmeaOut(message, sendNmeaOut) {
+    app.emit(sendNmeaOut, message.replace(/\r?\n|\r/, ' '));
   }
 
   function nmeaParser(message) {
     try {
-        const delta = parser.parse(message.trim())      
-        if (delta !== null) {
-          app.handleMessage(plugin.id,delta);
-        }
-    }
-    catch (e) {
-        console.error(`[error] ${e.message}`)
+      const delta = parser.parse(message.trim());
+      if (delta !== null) {
+        app.handleMessage(plugin.id, delta);
+      }
+    } catch (e) {
+      console.error(`[error] ${e.message}`);
     }
   }
 
   plugin.stop = function () {
+    let i;
     for (i = 0; i < numberMulticast; i++) {
-      if(socketMulticast[i]) {
+      if (socketMulticast[i]) {
         socketMulticast[i].close();
         socketMulticast[i] = null;
       }
-      if(socketUdp) {
+      if (socketUdp) {
         socketUdp.close();
         socketUdp = null;
       }
     }
-  }
+  };
 
   plugin.schema = {
-    type: "object",
+    type: 'object',
     properties: {
       interfaceIP: {
-        type: 'string',  
+        type: 'string',
         title: 'LAN IP address connected to IEC61162-450 network. Internet connection via IEC61162-460 secure gateway',
-        default: '192.168.0.x'
+        default: '192.168.0.x',
       },
       sendNmeaOut: {
         type: 'string',
         title: 'Send nmea0183 out',
         description: 'Received data is forwarded to address. E.g. localhost.',
-        default: 'nmea0183out'
+        default: 'nmea0183out',
       },
       sendAddress: {
         type: 'string',
         title: 'Output address',
         description: 'Received data is forwarded to address. E.g. localhost.',
-        default: 'localhost'
+        default: 'localhost',
       },
       sendPort: {
         type: 'number',
         title: 'Output port',
         description: 'Output port for UDP data. E.g. multicast data from port 60001 forwarded to localhost 6001',
-        default: 6000
+        default: 6000,
       },
       multicast: {
-        type: "array",
-        title: "IEC61162-450 client",
+        type: 'array',
+        title: 'IEC61162-450 client',
         description: 'Multicast receiver for IEC61162-450 UDP data and proxy',
         items: {
-          type: "object",
+          type: 'object',
           required: [],
           properties: {
             multicastAddress: {
               type: 'string',
               title: 'Multicast address to listen',
               description: 'The normally used range in 239.192.0.1-8, full range in 1-64.',
-              default: '239.192.0.x'
+              default: '239.192.0.x',
             },
             multicastPort: {
               type: 'number',
               title: 'Port to listen',
               description: 'The normal used range in 60001-60008 for addresses mentioned above.',
-              default: 60001
-            }
-          }
-        }
-      }
-    }
-  }
+              default: 60001,
+            },
+          },
+        },
+      },
+    },
+  };
 
-  return plugin
-}
+  return plugin;
+};
